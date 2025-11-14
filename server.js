fe28c26e48411e9bb14e79f2fcd8eba06b495c93
Tobiloba David Igiebor <77983084+omini25@users.noreply.github.com>
2025-11-14 07:14:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(express.json({limit: '50mb'}));
 
